6b2a6b5a062f0d4b3696627cdda816699195301b
humbertogouveiageekie <humberto.gouveia@geekie.com.br>
2024-12-19 17:14:36 -0300

    feat: Branch de exercício

--- cypress/e2e/ambiente.cy.js
+++
@@ -1,7 +0,0 @@
-
-it('Validar a presença do bit 0501 na trasação', () => {
-  cy.readFile('cypress/fixtures/log.txt', 'utf8').then((data) => {
-    const cleanedData = data.trim().toString();
-    expect(cleanedData).to.contain('0501');
-  });
-})
--- cypress/e2e/validacaoDinamica.cy.js
+++ cypress/e2e/validacaoDinamica.cy.js
@@ -1,23 +1,20 @@
 /// <reference types="cypress" />
 
-// Importando os arquivos JSON corretamente
-import categoryData from '../fixtures/api_categories.json' // Importa todo o objeto do arquivo JSON
+import categoryData from '../fixtures/api_categories.json'
 import dados from '../fixtures/data.json'
 import { homePage } from '../support/pages/home.page.js'
 
 describe('Testando validações dinâmicas', () => {
 
-  // Antes de cada teste, faz login com as credenciais do JSON
   beforeEach(() => {
-    cy.login(dados.email, dados.senha)  // Acessa as propriedades email e senha corretamente
+    cy.login(dados.email, dados.senha)
   })
 
-  // Itera sobre cada categoria presente no array de categorias dentro do JSON
-  categoryData[0].categories.forEach((category) => {  // Acessa a propriedade 'categories' dentro do objeto
+  categoryData[0].categories.forEach((category) => { 
     it(`Validação categoria ${category.name}`, () => {
       homePage.abrirMenuInformandoOpcao('Browse')
       homePage.openCategories()
-      homePage.categories().should('contain.text', category.name)  // Corrigido para 'category.name'
+      homePage.categories().should('contain.text', category.name)
     })
   })
 
